889e9a989ce3a66c19c42e54c1dd8d678fb79e0f
Joao Gilberto Magalhaes <joao@byjg.com.br>
2019-03-26 23:49:03 -0500

    Added multiple SSH_PUBLIC_KEY

--- README.md
+++ README.md
@@ -27,4 +27,19 @@ services:
       - 2222:22
 ```
 
+# Passing more than one public key
+
+Just add `SSH_PUBLIC_KEY` variables followed by 1, 2, 3. 
+
+```bash
+docker run \
+   --rm \
+   -p 2222:22 \
+   -e SSH_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAB...oOZRfcqYs4JPQgxpZ public-key" \
+   -e SSH_PUBLIC_KEY1="ssh-rsa CCCCCC4AbCed12ECCCCCCCCCCCCC...hja9ajhskjhasjkla public-key" \
+   -e SSH_PUBLIC_KEY2="ssh-rsa DDDDE5ua8AjajFDDDDDDDDDDDDDD...aha2ujaja81jggaaa public-key" \
+   --name openssh-server \
+   -d byjg/openssh-server
+```
+
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "$SSH_PUBLIC_KEY" ]
+if [[ -z "$SSH_PUBLIC_KEY" ]]
 then
     echo "You have to define the 'SSH_PUBLIC_KEY' env var"
     exit 1
@@ -10,8 +10,16 @@ HOME_SSH=/root/.ssh
 
 mkdir -p "$HOME_SSH"
 
-echo $SSH_PUBLIC_KEY > $HOME_SSH/authorized_keys
-chmod 400 $HOME_SSH/authorized_keys
+i=""
+VAR="SSH_PUBLIC_KEY$i"
+while [[ ! -z `eval 'echo $'"${VAR}"` ]]
+do
+    echo `eval 'echo $'"${VAR}"` >> ${HOME_SSH}/authorized_keys
+    i=$((i+1))
+    VAR="SSH_PUBLIC_KEY$i"
+done
+
+chmod 400 ${HOME_SSH}/authorized_keys
 
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd -D -E /proc/1/fd/1
